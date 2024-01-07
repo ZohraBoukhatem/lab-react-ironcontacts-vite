7eeed4cbb607f6eed43dcadd1ef27cf963d45917
Zohra <zohra.boukhatem1@gmail.com>
2024-01-07 22:30:31 +0100

    render contacts, conditional render for awards, add delete functionality, add random contact functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,62 @@
+import { useContext, useState } from "react";
 import "./App.css";
+import allContacts from "./contacts.json";
 
 function App() {
+  const [contacts, setContacts] = useState(allContacts.slice(0, 5));
+
+
+  const handleAdd = () => {
+    const randomContact =  allContacts[Math.floor(Math.random() * allContacts.length)]
+    console.log("added: ", randomContact)
+    !contacts.includes(randomContact) && setContacts([...contacts, randomContact]) 
+  }
+
+  const handleDelete = (id) => {
+   const filteredContacts = contacts.filter((contact) => {
+      return contact.id != id
+    })
+  setContacts(filteredContacts)
+  console.log("deleted contact with id: ",id)
+  }
+
+  const sortByName = () => {}
+  const sortByPopularity = () => {}
+  
   return (
-    <div className="App">
-      <h1>LAB | React IronContacts</h1>
-    </div>
+    <>
+    <button onClick={handleAdd}>Add random contact</button>
+    <button onClick={sortByName}>Sort by name</button>
+    <button onClick={sortByPopularity}>Sort by popularity</button>
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won an oscar</th>
+            <th>Won an emmy</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => {
+            return (
+                <tr key={contact.id}>
+                  <td>
+                    <img src={contact.pictureUrl} alt="contact picture" />
+                  </td>
+                  <td>{contact.name}</td>
+                  <td>{contact.popularity}</td>
+                  {contact.wonOscar && <td>🏆</td>}
+                  {contact.wonEmmy && <td>🌟</td> }
+                  <td><button onClick={() => handleDelete(contact.id)}>Delete</button></td>
+                </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
 }
 
